b18a8af9d4b2dbf886de4ee435fb4bc6ed0ae8b9
Mehmet <mehmetong45@gmail.com>
2023-05-29 14:33:23 +0300

    First Commit for LazyGrid in Jetpack Compose

---
+++ app/src/main/java/com/techmania/lazygridexample/CountryModel.kt
@@ -0,0 +1,10 @@
+package com.techmania.lazygridexample
+
+data class CountryModel(
+
+    val countryId : Int,
+    val countryName : String,
+    val countryDetail : String,
+    val countryImage : Int
+
+)
